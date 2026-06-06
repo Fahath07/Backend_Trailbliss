94a8889a872fc2365eddb949cfc078652b2d35c0
Fahath07 <fahathalsalam.s2024cse@sece.ac.in>
2026-06-06 16:00:37 +0530

    Switch SMTP to port 587 STARTTLS for Render network compatibility

--- Controllers/EnquiryController.js
+++ Controllers/EnquiryController.js
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer');
 function createTransporter() {
   return nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
     tls: { rejectUnauthorized: false },
   });
