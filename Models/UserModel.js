7b4b6dd78b675ae4650d5b88358ae183af6f42f7
Fahath07 <fahathalsalam.s2024cse@sece.ac.in>
2026-06-06 15:10:01 +0530

    User registration error

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -71,16 +71,10 @@ const userSchema = new mongoose.Schema({
 });
 
 // Encrypt password before saving
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  
-  try {
-    const salt = await bcrypt.genSalt(12);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+userSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Compare password method
